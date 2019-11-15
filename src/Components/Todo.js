b30af73d645681d2e694fcf4a86a6e58323f5ada
UpperLEFTY <upperlefty@zoho.eu>
2019-11-15 04:02:19 +0100

    changes

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Todo.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 
 function Task({ task, index, completeTask, removeTask }) {
